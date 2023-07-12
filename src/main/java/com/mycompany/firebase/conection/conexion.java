ab747705d2c20a7e469f291f5307439312d40586
JaviiR <alexircaa@gmail.com>
2023-07-11 19:08:16 -0500

    primera version subir archivos al storage(desordenado)

---
+++ src/main/java/com/mycompany/firebase/conection/conexion.java
@@ -0,0 +1,27 @@
+package com.mycompany.firebase.conection;
+
+import java.io.FileInputStream;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class conexion {
+
+    public static void Conectar() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("tutorial.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            System.out.println("Conexion exitosa");
+        } catch (Exception e) {
+            System.out.println("Conexion fallida por: "+e.getMessage());
+        }
+
+    }
+
+}
